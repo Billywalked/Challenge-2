b45318d5c3f5da9aa4b3e304f563726ef222850f
Prince Robinson <robinsonp@roverkids.org>
2023-12-13 14:18:03 -0500

    Step 2-5

--- Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -26,7 +26,8 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        int animalIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[animalIndex], spawnPos, ballPrefabs[animalIndex].transform.rotation);
     }
 
 }
